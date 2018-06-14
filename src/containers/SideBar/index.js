80dcc7bd1a06e1ea08635f81c4433c12182a37f9
Leonardo Cardoso <leocardosoti@gmail.com>
2018-06-14 14:13:45 -0300

    using apollo

--- src/containers/SideBar/index.js
+++ src/containers/SideBar/index.js
@@ -1,10 +1,22 @@
 import React from 'react';
+import { graphql } from 'react-apollo';
+import gql from 'graphql-tag';
 import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import './index.scss';
 import DevIcon from './../../components/Icons/dev-icon';
 import FaIcon from './../../components/Icons/fa-icon';
-import { withCategories } from '../../contexts/Categories';
+
+const CATEGORIES_QUERY = gql`
+  # 2
+  query CategoriesQuery {
+  categories(limit: 1000) {
+    id
+    title
+    icon
+  }
+}
+`;
 
 const RenderCategoryItem = ({ category, closeSideBar }) => (
   <li >
@@ -14,7 +26,7 @@ const RenderCategoryItem = ({ category, closeSideBar }) => (
       onClick={() => closeSideBar()}
       onKeyPress={() => closeSideBar()}
     >
-      {category.icon && <DevIcon icon={category.icon} color={category.iconColor} /> }
+      {category.icon && <DevIcon icon={category.icon} color={category.iconColor} />}
       {category.title}
     </Link>
   </li>
@@ -26,40 +38,54 @@ RenderCategoryItem.propTypes = {
 };
 
 
-const SideBar = ({ closeSideBar, categories, filterCategory }) => (
-  <aside className="sidebar">
-    <div className="sidebar-inner">
-      <input
-        className="search"
-        placeholder=""
-        onChange={event => filterCategory(event.target.value)}
-      />
-      <ul>
-        <li>
-          <Link
-            className="item"
-            to="/category/all"
-            onClick={() => closeSideBar()}
-            onKeyPress={() => closeSideBar()}
-          ><FaIcon icon="fa-code" />All Courses
-          </Link>
-        </li>
-        {categories.map(category => (
-          <RenderCategoryItem
-            category={category}
-            closeSideBar={closeSideBar}
-            key={category.title}
-          />
-        ))}
-      </ul>
-    </div>
-  </aside>
-);
+const SideBar = ({ closeSideBar, categoriesQuery, filterCategory }) => {
+  if (categoriesQuery && categoriesQuery.loading) {
+    return <div>Loading</div>;
+  }
 
-export default withCategories(SideBar);
+  // 2
+  if (categoriesQuery && categoriesQuery.error) {
+    return <div>Error</div>;
+  }
+
+  const { categories } = categoriesQuery;
+
+  return (
+    <aside className="sidebar">
+      <div className="sidebar-inner">
+        <input
+          className="search"
+          placeholder=""
+          onChange={event => filterCategory(event.target.value)}
+        />
+        <ul>
+          <li>
+            <Link
+              className="item"
+              to="/category/all"
+              onClick={() => closeSideBar()}
+              onKeyPress={() => closeSideBar()}
+            ><FaIcon icon="fa-code" />All Courses
+            </Link>
+          </li>
+          {categories.map(category => (
+            <RenderCategoryItem
+              category={category}
+              closeSideBar={closeSideBar}
+              key={category.title}
+            />
+          ))}
+        </ul>
+      </div>
+    </aside>
+  );
+};
+
+
+export default graphql(CATEGORIES_QUERY, { name: 'categoriesQuery' })(SideBar);
 
 SideBar.propTypes = {
   closeSideBar: PropTypes.func.isRequired,
   filterCategory: PropTypes.func.isRequired,
-  categories: PropTypes.arrayOf(Object).isRequired
+  // categories: PropTypes.arrayOf(Object).isRequired
 };
--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { ApolloProvider } from 'react-apollo';
+import { ApolloClient } from 'apollo-client';
+import { HttpLink } from 'apollo-link-http';
+import { InMemoryCache } from 'apollo-cache-inmemory';
 import { register } from './registerServiceWorker';
 import Container from './containers/container';
 import ContextProvider from './contexts';
@@ -11,11 +15,23 @@ import './index.scss';
 import './../node_modules/font-awesome/css/font-awesome.min.css';
 import './../node_modules/flag-icon-css/css/flag-icon.min.css';
 
+
+const httpLink = new HttpLink({ uri: 'http://localhost:4000' });
+
+// 3
+const client = new ApolloClient({
+  link: httpLink,
+  cache: new InMemoryCache()
+});
+
+
 register();
 
 ReactDOM.render(
-  <ContextProvider>
-    <Container />
-  </ContextProvider>
+  <ApolloProvider client={client}>
+    <ContextProvider>
+      <Container />
+    </ContextProvider>
+  </ApolloProvider>
   , document.getElementById('app')
 );
